4f74ddd03d84ba7c0e630423fe0a93885e9f3d9c
Koichi Murase <myoga.murase@gmail.com>
2021-11-17 17:45:17 +0900

    add "phys/notation"

--- make.sh
+++ make.sh
@@ -106,10 +106,10 @@ generate phys/glauber.html グラウバー模型
 generate phys/fluid.html 流体力学の基本
 generate phys/standard-error.html 標準誤差の評価
 generate phys/scatter-variable.html 加速器実験の粒子の運動量
-
 generate phys/cumulant.html 'キュムラント (統計)'
 generate phys/exponential-family.html '指数型分布族 (統計)'
 
 generate aghtex/test-bigl.html 'test \bigl, \bigr'
 
 generate text/202111-quantum-neq.html '量子と非平衡の物理 (東京大学出版会)'
+generate phys/notation.html '記号と用語の定義'
